5596d3a991e0f8631067ab249006978a58c87c7e
marcosaugustomodesto <marcosaugustomodesto15@gmail.com>
2019-05-17 16:58:12 -0300

    Starting Implementation of The Burger Builder Container

---
+++ src/containers/BurguerBuilder/BurguerBuilder.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import Auxilary from '../../hoc/Auxilary';
+class BurguerBuilder extends Component {
+
+    render(){
+        return (
+            <Auxilary>
+                <div>Burger</div>
+                <div>Builder Controls</div>
+            </Auxilary>
+        );
+    }
+}
+
+export default BurguerBuilder;
